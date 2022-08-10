c74e68d06be3a239ea510b463f6f001a6c512afc
Jean Vitor <jeanvdf123@gmail.com>
2022-08-10 09:45:19 -0300

    VendedorDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Department;
 import model.entities.Vendedor;
 
@@ -10,7 +12,11 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
+		
 		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
 		System.out.println(obj);
 		System.out.println(vendedor);
 	}
